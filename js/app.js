046a8cc1f395e0a076ecaad4cef6e0d181e4e3e7
Sandhage <kylesandhage@gmail.com>
2015-10-26 18:10:33 -0400

    Set up variable and first function of new-game button; began work on input variable comparison.

--- js/app.js
+++ js/app.js
@@ -12,6 +12,22 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+	// Randomly Generate Number, Store as Variable
+	var hcAnswer = Math.floor(Math.random()*100) + 1;
+	console.log(hcAnswer);
+
+  	// New Game Function
+  	document.getElementById("newGame").addEventListener("click", function() {
+  		hcAnswer = Math.floor(Math.random()*100) + 1;
+  		console.log(hcAnswer)
+  	});
+
+  	// Guess Events and Functions
+  	document.getElementById("guessButton").addEventListener("click", function() {
+  		var newGuess = document.querySelector("input").val();
+  		console.log(newGuess);
+  	})
+
 });
 
 
