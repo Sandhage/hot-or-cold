b54187184f411bc5bfb226803cb31c95957c3de0
sandhage <kylesandhage@gmail.com>
2015-10-29 14:52:38 -0400

    Made global guess/answer variables, made guess-list functional, started working on feedback header.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,13 @@
 
 $(document).ready(function(){
 	
+  // Global Variables
+  var newGuess = null;
+  var oldGuess = null;
+  var hcAnswer = Math.floor(Math.random()*100) + 1;
+    console.log(hcAnswer);
+  var guessCount = 0;
+
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
@@ -12,10 +19,6 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
-	// Randomly Generate Number, Store as Variable
-	var hcAnswer = Math.floor(Math.random()*100) + 1;
-	console.log(hcAnswer);
-
   	// New Game Function
   	document.getElementById("newGame").addEventListener("click", function() {
   		hcAnswer = Math.floor(Math.random()*100) + 1;
@@ -23,11 +26,38 @@ $(document).ready(function(){
   	});
 
   	// Guess Events and Functions
-  	document.getElementById("guessButton").addEventListener("click", function() {
-  		var newGuess = document.querySelector("input").val();
-  		console.log(newGuess);
+  	document.getElementById("guessButton").addEventListener("click", function(e) {
+      e.preventDefault();
+
+      guessCount++;
+      document.getElementById('count').innerHTML = guessCount;
+
+      oldGuess = newGuess;
+      console.log('oldGuess: ' + oldGuess);
+
+      newGuess = document.querySelector("#userGuess").value;
+  		console.log('new(est)Guess: ' + newGuess);
+
+      document.getElementById('guessList').innerHTML += '<li>' + newGuess + '</li>';
+
+      var difference = Math.abs(newGuess - hcAnswer);
+      var feedback = null;
+
+      if ( newGuess == hcAnswer ) {
+        feedback = 'You win! Wow!';
+      } else if ( difference <= 10 ) {
+        feedback = 'Is that your mixtape? THATS SUPER HOT FIRE';
+      } else if ( difference <= 20 ) {
+        alert('Pretty hot!!');
+      } else if ( difference <= 30 ) {
+        alert('Tepid. Lukewarm. Try again.')
+      }
+
+      document.getElementById('feedback').innerHTML = feedback;
   	})
 
 });
 
 
+// THINGS TO DO WHEN REFACTORING:
+// 1. make Math.floor a function to keep it neat and tidy.
